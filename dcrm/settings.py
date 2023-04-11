8631a947fa8b02f6e6a13dfc4cf98f7e3131f945
amaniMagdi <amanim.farag@gmail.com>
2023-04-11 11:58:05 +0200

    add login, logout and registration

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -86,6 +86,7 @@ DATABASES = {
 }
 
 #created superuser: admin with password:admin123
+#created normal user: yahia.all password:yahia1234567
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
