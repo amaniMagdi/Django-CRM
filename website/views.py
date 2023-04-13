72401a86b0d5ab4615ea0b89b1c98f9d14a396bd
amaniMagdi <amanim.farag@gmail.com>
2023-04-13 15:10:35 +0200

    show individual record and enable user to delete it

--- website/views.py
+++ website/views.py
@@ -43,6 +43,29 @@ def register_user(request):
         form = SignUpForm() 
         return render(request, 'register.html', {'form':form})
     return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "You must be logged in firstly...")
+        return redirect('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record deleted successfully...")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to do that...")
+        return redirect('home')
+
+
+
+
+
            
 
     
